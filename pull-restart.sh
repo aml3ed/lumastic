8ceb20ad73a133e3645788c59f17410325505e04
aml3ed <aml3ed@virginia.edu>
2018-10-08 20:12:52 -0400

    Updated sh file

--- pull-restart.sh
+++ pull-restart.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 docker-compose -f docker-compose-prod.yml down
 git pull
+docker-compose -f docker-compose-prod.yml run web db:migrate
 docker-compose -f docker-compose-prod.yml build
 docker-compose -f docker-compose-prod.yml up -d
