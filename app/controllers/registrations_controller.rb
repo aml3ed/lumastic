38f45ecba658ba24e18745a8042760c62fe29e4f
David Clark <dc7hk@virginia.edu>
2018-11-01 00:12:33 -0400

    Add new user to Lumastic Community

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -5,6 +5,15 @@ class RegistrationsController < Devise::RegistrationsController
   include ApplicationHelper
   layout 'application'
 
+  def create
+    super
+
+    if Community.first.present?
+      community = Community.where(name: "Lumastic Community").first
+      Membership.create!(community: community, user: resource)
+    end
+  end
+
   def sign_up_params
     params.require(:user).permit(:display_name, :email, :password, :password_confirmation)
   end
