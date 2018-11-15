1ecaebc21b05e8ab32ed6a9df560ecd830d12e05
aml3ed <aml3ed@virginia.edu>
2018-11-14 21:13:09 -0500

    Only saves valid urls now!

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -6,6 +6,6 @@ class Lesson < ApplicationRecord
 
   # Validations
   validates :title, presence: true
-  validates :video_url, presence: true, uniqueness: false
+  validates :video_url, presence: true, uniqueness: false, format: {with: /\A(http(s)?:\/\/)?((w){3}.)?youtu(be|.be)?(\.com)?\/.+\z/}
   validates :position, presence: true, uniqueness: { scope: :course }
   end
