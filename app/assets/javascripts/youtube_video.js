b737071bb4041cd17f8bf3562eacd994090767a9
aml3ed <aml3ed@virginia.edu>
2018-11-24 23:07:13 -0500

    Finished update to lesson show for upvote downvote

--- app/assets/javascripts/youtube_video.js
+++ app/assets/javascripts/youtube_video.js
@@ -1,6 +1,7 @@
 function containerFadeOut() {
-    $('.vid-overlay-container:first').fadeOut(400, function(){
-        $(this).hide()
+    $('.vid-overlay-container:first').fadeOut(400, function() {
+        $(this).css({opacity: 0, display: 'flex'});
+        $(this).remove();
     });
     $('.vid-iframe-container')[0].lastElementChild.style.filter = "none";
 }
@@ -8,61 +9,11 @@ function containerFadeOut() {
 function playButton() {
     containerFadeOut();
     player.play();
-
-    $('.vid-button').each(function() {
-        console.log('got here');
-       $(this).attr('data-type', 'out');
-    });
-    $('.vid-text-overlay').html('How confident do you feel now?');
-}
-
-var outClicked = false;
-function bringBackButtons() {
-    if(player.get_duration() - player.get_current_time() > 20 || outClicked)
-        return;
-    $('.vid-overlay-container:first').fadeIn(400, function(){
-        $(this).show()
-        outClicked = true;
-    });
 }
 
 function vidbuttonOnClick() {
-    var inDone = false;
-    $(".vid-button").click(function() {
-        var type = "";
-        if(inDone) {
-            type = 'out';
-            containerFadeOut();
-        } else {
-            type = 'in';
-            inDone = true;
-            playButton();
-        }
-        switch (this.id) {
-            case 'red':
-                // do something
-                Rails.ajax({
-                    url: $(this).data("url"),
-                    type: "PATCH",
-                    data: "color=red&type="+type
-                });
-                break;
-            case 'blue':
-                // do something
-                Rails.ajax({
-                    url: $(this).data("url"),
-                    type: "PATCH",
-                    data: "color=blue&type="+type
-                })
-                break;
-            case 'green':
-                // do something
-                Rails.ajax({
-                    url: $(this).data("url"),
-                    type: "PATCH",
-                    data: "color=green&type="+type
-                })
-                break;
-        }
+    $("#play-btn").click(function() {
+        playButton();
+
     });
 }
