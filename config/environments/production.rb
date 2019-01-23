140db7b7cc3dedfbe290c0d3724b9be3ab51fff6
aml3ed <aml3ed@virginia.edu>
2019-01-23 11:17:11 -0500

    Added secret base

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,7 +21,6 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
-  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -33,7 +32,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['ASSET_HOST']
+  # config.action_controller.asset_host = ENV['ASSET_HOST']
 
   # Specifies the header that your server uses for sending files.
   # Specifies the header that your server uses for sending files.
