a5355376c2915b4c4625c9f3725048530fe7be25
aml3ed <aml3ed@virginia.edu>
2019-01-23 11:18:04 -0500

    Added secret base

--- bash/push-to-heroku-dev.sh
+++ bash/push-to-heroku-dev.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-docker build --build-arg secret_key_base=${@} -f Dockerfile.prod -t registry.heroku.com/lumastic-dev/web:latest .
-
+docker build --build-arg secret_key_base=${@} -f Dockerfile.prod -t registry.heroku.com/lumastic-dev/web .
+docker push registry.heroku.com/lumastic-dev/web
