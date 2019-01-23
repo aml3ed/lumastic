7d2791cb02e8678dc9144399b781b09265f81f1c
aml3ed <aml3ed@virginia.edu>
2019-01-23 11:24:02 -0500

    Fixed it

--- bash/push-to-heroku-dev.sh
+++ bash/push-to-heroku-dev.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 docker build --build-arg secret_key_base=${@} -f Dockerfile.prod -t registry.heroku.com/lumastic-dev/web .
 docker push registry.heroku.com/lumastic-dev/web
+heroku container:release web --app lumastic-dev
