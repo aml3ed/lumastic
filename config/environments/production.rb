4380745cf7b5907154c67ec28f1709f4f209c585
aml3ed <aml3ed@virginia.edu>
2019-01-22 22:48:40 -0500

    Another one

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -21,6 +21,7 @@ Rails.application.configure do
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = true
+  config.assets.digest = true
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
